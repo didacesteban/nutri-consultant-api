53864d0c29c52d672875f867ebd74717d1c2e410
didac esteban <didacesteban@gmail.com>
2019-11-29 13:02:40 +0100

    wip

--- contact/index.php
+++ contact/index.php
@@ -1,46 +1,44 @@
 <?php
-
 header("Access-Control-Allow-Origin: *");
 $rest_json = file_get_contents("php://input");
 $_POST = json_decode($rest_json, true);
-$email = $_POST['email'];
 
-echo json_encode(["sent" => false, "message" => $email]);
 // if (empty($_POST['fname']) && empty($_POST['email'])) die();
-//
-// if ($_POST)
-// 	{
-//
-// 	// set response code - 200 OK
-//
-// 	http_response_code(200);
-// 	$subject = $_POST['fname'];
-// 	$to = "me@malith.pro";
-// 	$from = $_POST['email'];
-//
-// 	// data
-//
-// 	$msg = $_POST['number'] . $_POST['message'];
-//
-// 	// Headers
-//
-// 	$headers = "MIME-Version: 1.0\r\n";
-// 	$headers.= "Content-type: text/html; charset=UTF-8\r\n";
-// 	$headers.= "From: <" . $from . ">";
-// 	mail($to, $subject, $msg, $headers);
-//
-// 	// echo json_encode( $_POST );
-//
-// 	echojson_encode(array(
-// 		"sent" => true
-// 	));
-// 	}
-//   else
-// 	{
-//
-// 	// tell the user about error
-//
-// 	echojson_encode(["sent" => false, "message" => "Something went wrong"]);
-// 	}
+
+if ($_POST)
+	{
+
+	// set response code - 200 OK
+
+	http_response_code(200);
+	// $subject = $_POST['fname'];
+	// $to = "me@malith.pro";
+	// $from = $_POST['email'];
+  //
+	// // data
+  //
+	// $msg = $_POST['number'] . $_POST['message'];
+  //
+	// // Headers
+  //
+	// $headers = "MIME-Version: 1.0\r\n";
+	// $headers.= "Content-type: text/html; charset=UTF-8\r\n";
+	// $headers.= "From: <" . $from . ">";
+	// mail($to, $subject, $msg, $headers);
+
+	// echo json_encode( $_POST );
+
+	echo json_encode(array(
+		"sent" => true,
+    "message" => $_POST['email']
+	));
+	}
+  else
+	{
+
+	// tell the user about error
+
+	echo json_encode(["sent" => false, "message" => "Something went wrong"]);
+	}
 
 ?>
