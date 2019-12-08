3369635d3dfaad72ea14f0c49b109c2fbcbc77ec
Didac Esteban Andres <didacesteban@gmail.com>
2019-12-08 15:46:49 +0100

    trying to send email

--- contact/index.php
+++ contact/index.php
@@ -3,7 +3,7 @@ header("Access-Control-Allow-Origin: *");
 $rest_json = file_get_contents("php://input");
 $_POST = json_decode($rest_json, true);
 
-// if (empty($_POST['fname']) && empty($_POST['email'])) die();
+if (empty($_POST['email']) && empty($_POST['message'])) die();
 
 if ($_POST)
 	{
@@ -11,27 +11,30 @@ if ($_POST)
 	// set response code - 200 OK
 
 	http_response_code(200);
-	// $subject = $_POST['fname'];
-	// $to = "me@malith.pro";
-	// $from = $_POST['email'];
-  //
-	// // data
-  //
-	// $msg = $_POST['number'] . $_POST['message'];
-  //
-	// // Headers
-  //
-	// $headers = "MIME-Version: 1.0\r\n";
-	// $headers.= "Content-type: text/html; charset=UTF-8\r\n";
-	// $headers.= "From: <" . $from . ">";
-	// mail($to, $subject, $msg, $headers);
+	$subject = $_POST['subject'];
+	$to = "didacesteban@gmail.com";
+	$from = $_POST['email'];
+  
+	// data
+  
+	$msg = $_POST['message'];
+  
+	// Headers
+  
+	$headers = "MIME-Version: 1.0\r\n";
+	$headers.= "Content-type: text/html; charset=UTF-8\r\n";
+	$headers.= "From: <" . $from . ">";
+	mail($to, $subject, $msg, $headers);
 
+	echo json_encode(["sent" => true, "message" => "Email sent"]);
 	// echo json_encode( $_POST );
 
-	echo json_encode(array(
-		"sent" => true,
-    "message" => $_POST['email']
-	));
+	// echo json_encode(array(
+	// 	"sent" => true,
+	// 	"email" => $_POST['email'],
+	// 	"subject" => $_POST['subject'],
+	// 	"message" => $_POST['message']
+	// ));
 	}
   else
 	{
